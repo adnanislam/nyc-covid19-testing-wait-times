352beb2ccf477da9e3996569415233213e277941
naan <adnansislam@protonmail.com>
2021-01-17 17:13:31 -0500

    Create columnDefs and rowData

--- dashboard.js
+++ dashboard.js
@@ -1,3 +1,10 @@
+let columnDefs = [
+  {'field': 'clinic'},
+  {'field': 'reported time'},
+  {'field': 'wait time'}
+]
+
 d3.csv("https://raw.githubusercontent.com/astoria-tech/nyc-covid19-testing-wait-times/main/wait-times.csv").then(function(data) {
-  console.log(data);
+  let rowData = data.map(row => ({'clinic': row['clinic'], 'reported time': row['reported_time_human'], 'wait time': row['wait_time_minutes']}))
+  console.log(rowData);
 });
