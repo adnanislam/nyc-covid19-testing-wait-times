7d5b3f5ed78bd026fb79851450cf051519bd7d81
naan <adnansislam@protonmail.com>
2021-01-17 17:19:08 -0500

    Display grid with data

--- dashboard.js
+++ dashboard.js
@@ -6,5 +6,11 @@ let columnDefs = [
 
 d3.csv("https://raw.githubusercontent.com/astoria-tech/nyc-covid19-testing-wait-times/main/wait-times.csv").then(function(data) {
   let rowData = data.map(row => ({'clinic': row['clinic'], 'reported time': row['reported_time_human'], 'wait time': row['wait_time_minutes']}))
-  console.log(rowData);
+  let gridOptions = {
+    columnDefs: columnDefs,
+    rowData: rowData
+  }
+
+  let eGridDiv = document.querySelector('#myGrid');
+  new agGrid.Grid(eGridDiv, gridOptions);
 });
